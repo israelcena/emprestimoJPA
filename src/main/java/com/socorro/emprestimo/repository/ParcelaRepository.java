76a5a45059b6c03b8a06ddd679e6fc0f1db28c85
israelcena <israelsjm@gmail.com>
2022-03-24 20:01:53 -0300

    fix ParcelaRepository types

--- src/main/java/com/socorro/emprestimo/repository/ParcelaRepository.java
+++ src/main/java/com/socorro/emprestimo/repository/ParcelaRepository.java
@@ -1,8 +1,9 @@
 package com.socorro.emprestimo.repository;
 
+import com.socorro.emprestimo.model.Parcela;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ParcelaRepository extends JpaRepository {
+public interface ParcelaRepository extends JpaRepository<Parcela, Integer> {
 }
